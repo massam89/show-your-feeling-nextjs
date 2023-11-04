e0fc314ee1d0d46322ade6a225ed3ffdfd75d1f3
Masoud Sam <mas.sam1368@gmail.com>
2023-11-04 15:15:42 +0330

    change sth

--- app/[phoneNumberBase64]/page.js
+++ app/[phoneNumberBase64]/page.js
@@ -1,25 +1,23 @@
 'use client'
+
 import { Base64 } from "js-base64";
 import { useParams } from "next/navigation"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const {phoneNumberBase64} = useParams()
+  const [isShow, setIsShow] = useState(false)
   const phoneNumber = Base64.decode(phoneNumberBase64)
 
   useEffect(() => {
     if(confirm('Do you like to get number?')){
-      const phoneLink = document.createElement('a');
-      phoneLink.href = `tel:${phoneNumber}`;
-      document.body.appendChild(phoneLink);
-      phoneLink.click();
-      document.body.removeChild(phoneLink);
+      setIsShow(true)
     } else {
-      alert('Good luck!')
+      window.close()
     }
   }, [])
 
     return (
-      <></>
+      isShow && <a href={`tel:${phoneNumber}`}>{phoneNumber}</a>
     )
   }
