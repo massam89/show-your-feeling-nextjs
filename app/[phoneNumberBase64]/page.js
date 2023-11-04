a37bdfdd6d8c085862ef09f8b27a0c8533e2c97a
Masoud Sam <mas.sam1368@gmail.com>
2023-11-04 14:57:09 +0330

    change sth

--- app/[phoneNumberBase64]/page.js
+++ app/[phoneNumberBase64]/page.js
@@ -1,22 +1,21 @@
 'use client'
 import { Base64 } from "js-base64";
 import { useParams } from "next/navigation"
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 export default function Page() {
-  const linkRef = useRef()
   const {phoneNumberBase64} = useParams()
   const phoneNumber = Base64.decode(phoneNumberBase64)
 
   useEffect(() => {
     if(confirm('Do you like to get number?')){
-      linkRef.current.click()
+      window.location.href = `tel:${phoneNumber}`
     } else {
       alert('Good luck!')
     }
   }, [])
 
     return (
-      <a ref={linkRef} href={`tel:${phoneNumber}`}>{phoneNumber}</a>
+      <></>
     )
   }
